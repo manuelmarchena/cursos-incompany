1054c4739c024d667c50b296824520762d5af97a
manuelmarchena <manuelmarche34@gmail.com>
2025-06-23 16:22:06 -0300

    Se agrega Express

---
+++ express-api/index.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+
+// const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+
+    res.status(401).json({
+        ok: false,
+        msg: 'No hay token en la petición'
+    })
+
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port http://localhost:${port}`);
+})
